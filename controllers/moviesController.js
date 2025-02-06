ca05460cb182ee3ade9f298607fa0a1e2f5b41ee
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-02-06 16:55:56 +0100

    feat: add show with reviews

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -2,12 +2,41 @@ import connection from "../data/db.js";
 
 
 const index = (req, res) => {
-  res.send('Elenco film')
+  const sql = `SELECT * FROM movies`
+
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: err.message })
+    res.json(results)
+  })
 };
 
 const show = (req, res) => {
   const id = req.params.id
-  res.send(`Film con id = ${id}`)
+
+  const sql = ` SELECT * FROM movies WHERE id = ?`;
+
+  const sqlReviews = `SELECT R.*
+  FROM reviews R
+  LEFT JOIN movies M ON M.id = R.movie_id
+  WHERE M.id = ?`
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: err.message })
+    if (results.length === 0) return res.status(404).json({ error: 'Film non trovato' })
+
+    const movie = results[0];
+
+    connection.query(sqlReviews, [id], (err, resultsReview) => {
+      if (err) return res.status(500).json({ error: err.message })
+      if (resultsReview.length === 0) return res.status(404).json({ error: 'Recensione non trovata' })
+
+      movie.reviews = resultsReview;
+      res.json(movie)
+
+    })
+
+
+  })
 };
 
 export {
